a1c0dd07ad11c719941b738ff182a3c8104bbe2b
phapntm <phapntm@gmail.com>
2022-09-25 00:13:52 +0700

    init

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const proxy = "http://gate.smartproxy.com:10000";
 const originDomain = "wss://chat.1g88.vin";
 const WSS = new WebSocketServer({ port: 46578, path: "/chatHub" });
 WSS.on("connection", function connection(ws, request) {
-  //   var agent = new HttpsProxyAgent(proxy);
+  console.log(request.url);
+  let agent;
+  //   agent = new HttpsProxyAgent(proxy);
   const messageSaveOnNotConnectYet = [];
-  const clientWS = new WebSocket(originDomain + request.url);
+  const clientWS = new WebSocket(originDomain + request.url, {
+    agent,
+  });
   clientWS.on("message", (data) => {
     ws.send(data.toString());
   });
@@ -31,35 +35,4 @@ WSS.on("connection", function connection(ws, request) {
       console.log("error", error);
     }
   });
-  //   const urlWs = `wss://chat.herransmap.info/chatHub?access_token=${accessToken}`;
-  //   const messageSub = `{"protocol":"json","version":1}`;
-  //   let   messageDelay =[];
-  //   const wsClient = new WebSocket(urlWs, {agent });
-  //   wsClient.on("open", function open() {
-  //     console.log("Socket Connected");
-  //     console.log("messageDelay", messageDelay.length);
-  //     wsClient.send(messageSub);
-  //   });
-  //   wsClient.on("message", function message(data) {
-  //     //console.log(data.toString());
-  //     ws.send(
-  //       data
-  //         .toString()
-  //         .replace("tai88vin.link", "88vin.in")
-  //         .replace("tai88vin.link", "88vin.in")
-  //     );
-  //   });
-
-  //   // wsClient error
-
-  //   // on user client send message
-
-  //   ws.on("message", (message) => {
-  //     try {
-  //       wsClient.send(message.toString());
-  //     } catch (error) {
-  //       console.log('error',error);
-  //       messageDelay.push([message.toString()]);
-  //     }
-  //   });
 });
